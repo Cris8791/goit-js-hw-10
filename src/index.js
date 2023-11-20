66dbcf695514cd6782984c6c8fa7e873d6a3a746
Cris8791 <135622871+Cris8791@users.noreply.github.com>
2023-11-20 16:14:30 +0200

    commit_2_functional

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+
+document.addEventListener('DOMContentLoaded', async function () {
+  const breedSelect = document.querySelector('.breed-select');
+  const loader = document.querySelector('.loader');
+  const error = document.querySelector('.error');
+  const catInfo = document.querySelector('.cat-info');
+  const catImage = document.getElementById('cat-image');
+  const catName = document.getElementById('cat-name');
+  const catDescription = document.getElementById('cat-description');
+  const catTemperament = document.getElementById('cat-temperament');
+
+  try {
+    // Fetch and populate breeds
+    const breeds = await fetchBreeds();
+    breeds.forEach(breed => {
+      const option = document.createElement('option');
+      option.value = breed.id;
+      option.textContent = breed.name;
+      breedSelect.appendChild(option);
+    });
+
+    breedSelect.addEventListener('change', async function () {
+      const selectedBreedId = breedSelect.value;
+
+      // Show loader while fetching cat info
+      loader.style.display = 'block';
+      catInfo.style.display = 'none';
+      error.style.display = 'none';
+
+      try {
+        // Fetch and display cat info
+        const cat = await fetchCatByBreed(selectedBreedId);
+        catImage.src = cat.url;
+        catName.textContent = `Breed: ${cat.breeds[0].name}`;
+        catDescription.textContent = `Description: ${cat.breeds[0].description}`;
+        catTemperament.textContent = `Temperament: ${cat.breeds[0].temperament}`;
+
+        // Hide loader and show cat info
+        loader.style.display = 'none';
+        catInfo.style.display = 'block';
+      } catch (error) {
+        // Display error message
+        loader.style.display = 'none';
+        error.style.display = 'block';
+      }
+    });
+  } catch (error) {
+    // Display error message
+    loader.style.display = 'none';
+    error.style.display = 'block';
+  }
+});
